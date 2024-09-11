9b1fa2a49b8e7ed4c523fb3a3ab08f277a132047
reyhansaja <raihan.ts16b@gmail.com>
2024-09-11 16:27:57 +0700

    update bobot Siakad24

--- Siakad24.java
+++ Siakad24.java
@@ -1,32 +1,36 @@
 import java.util.Scanner;
 
-public class Siakad24{
-    public static void main (String args[]){
-        Scanner input = new Scanner (System.in);
-        String nama, nim;
-        char kelas;
+public class Siakad24 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String nama, nim, kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiUjianAkhir, nilaiAkhir;
+        
         System.out.print("Masukkan nama: ");
         nama = input.nextLine();
         System.out.print("Masukkan NIM: ");
         nim = input.nextLine();
         System.out.print("Masukkan Kelas: ");
-        kelas = input.nextLine().charAt(0);
+        kelas = input.nextLine();
         System.out.print("Masukkan no absen: ");
         absen = input.nextByte();
+        input.nextLine(); 
 
         System.out.print("Masukkan Nilai Kuis: ");
         nilaiKuis = input.nextDouble();
         System.out.print("Masukkan Nilai Tugas: ");
         nilaiTugas = input.nextDouble();
         System.out.print("Masukkan Nilai Ujian: ");
-        nilaiUjian =  input.nextDouble();
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
+        nilaiUjian = input.nextDouble();
+        System.out.print("Masukkan Nilai Ujian Akhir: ");
+        nilaiUjianAkhir = input.nextDouble();
+        
+        // Menghitung nilai akhir berdasarkan bobot
+        nilaiAkhir = (nilaiKuis * 0.20) + (nilaiTugas * 0.15) + (nilaiUjian * 0.30) + (nilaiUjianAkhir * 0.35);
 
-        System.out.println("Nama: " + nama + " NIM: " + nim );
-        System.out.println("Kelas: " + kelas + " Absen: " + absen );
+        System.out.println("Nama: " + nama + " NIM: " + nim);
+        System.out.println("Kelas: " + kelas + " Absen: " + absen);
         System.out.println("Nilai akhir: " + nilaiAkhir);
-
     }
 }
