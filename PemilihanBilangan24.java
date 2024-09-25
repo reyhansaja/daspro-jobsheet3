6bbb91327ae6f2f9b6bed21cea1e8e8d1252692b
reyhansaja <raihan.ts16b@gmail.com>
2024-09-25 15:28:47 +0700

    tugas 2.1 no 1

--- PemilihanBilangan24.java
+++ PemilihanBilangan24.java
@@ -1,16 +1,12 @@
 import java.util.Scanner;
-public class PemilihanBilangan24{
-    public static void main (String args[]){
-        Scanner sc = new Scanner (System.in);
+public class PemilihanBilangan24 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.print("Masukkan Sebuah Angka: ");
         int angka = sc.nextInt();
-        if (angka % 2 == 0)
-        {
-            System.out.println("Angka " + angka + " Termasuk bilangan genap");
-        }
-        else
-        {
-            System.out.println("Angka " + angka + " Termasuk bilangan ganjil");
-        }
+        
+        String hasil = (angka % 2 == 0) ? "genap" : "ganjil";
+        
+        System.out.println("Angka " + angka + " Termasuk bilangan " + hasil);
     }
 }
